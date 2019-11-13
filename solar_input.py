8d3bf805e120967dce7080c2efec15f142420e8f
Daniel <55478231+Striz-lab@users.noreply.github.com>
2019-11-13 11:18:44 +0300

    ы

--- solar_input.py
+++ solar_input.py
@@ -7,9 +7,7 @@ from solar_objects import Star, Planet
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
-
     Параметры:
-
     **input_filename** — имя входного файла
     """
 
@@ -37,25 +35,22 @@ def parse_star_parameters(line, star):
     """Считывает данные о звезде из строки.
     Входная строка должна иметь слеюущий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
     Пример строки:
     Star 10 red 1000 1 2 3 4
-
     Параметры:
-
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-    star.r = float(line[1])
-    star.color = line[2]
-    star.m = float(line[3])
-    star.x = float(line[4])
-    star.y = float(line[5])
-    star.Vx = float(line[6])
-    star.Vy = float(line[7])
+    star.r = float(line.split()[1].lower())
+    star.color = line.split()[2].lower()
+    star.m = float(line.split()[3].lower())
+    star.x = float(line.split()[4].lower())
+    star.y = float(line.split()[5].lower())
+    star.Vx = float(line.split()[6].lower())
+    star.Vy = float(line.split()[7].lower())
 
-    pass  # FIXME: not done yet done
+    #pass  # FIXME: not done yet done
 
 
 def parse_planet_parameters(line, planet):
@@ -63,24 +58,21 @@ def parse_planet_parameters(line, planet):
     Предполагается такая строка:
     Входная строка должна иметь слеюущий формат:
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
     Пример строки:
     Planet 10 red 1000 1 2 3 4
-
     Параметры:
-
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    planet.r = float(line[1])
-    planet.color = line[2]
-    planet.m = float(line[3])
-    planet.x = float(line[4])
-    planet.y = float(line[5])
-    planet.Vx = float(line[6])
-    planet.Vy = float(line[7])
-    pass  # FIXME: not done yet...
+    planet.r = float(line.split()[1].lower())
+    planet.color = line.split()[2].lower()
+    planet.m = float(line.split()[3].lower())
+    planet.x = float(line.split()[4].lower())
+    planet.y = float(line.split()[5].lower())
+    planet.Vx = float(line.split()[6].lower())
+    planet.Vy = float(line.split()[7].lower())
+    #pass  # FIXME: not done yet...
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -88,9 +80,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     Строки должны иметь следующий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Параметры:
-
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
